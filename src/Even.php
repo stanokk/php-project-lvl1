ca354206d765964ce18869c2739291e32466727c
Evgeny Kuzin <ekuzin@outlook.com>
2021-06-15 08:56:27 +0700

    fix Even.php

--- src/Even.php
+++ src/Even.php
@@ -11,33 +11,28 @@ function checkAnswer()
     $name = prompt('May I have your name?');
     line("Hello, %s!", $name);
     line('Answer "yes" if the number is even, otherwise answer "no".');
-    $number = mt_rand(1, 100);
-  
     for ($i = 0; $i <= 2; $i++) {
-	    $number = mt_rand(1, 100);
-	    line("Question: {$number}");
-	    $answer = prompt('Your answer');
+         $number = mt_rand(1, 100);
+         line("Question: {$number}");
+         $answer = prompt('Your answer');
 
-	 if ($number %2 === 0) {
+        if ($number % 2 === 0) {
             $correctAnswer = 'yes';
-         } elseif ($number %2 !== 0) {
+        } elseif ($number % 2 !== 0) {
             $correctAnswer = 'no';
-	 }
-	   if ($answer !== $correctAnswer) {
-                      switch($answer) {
-                      case 'yes':
-                              line("'yes' is wrong answer ;(. Correct answer was 'no'\n Let's try again, {$name}");
+        }
+        if ($answer !== $correctAnswer) {
+            switch ($answer) {
+                case 'yes':
+                    exit("'yes' is wrong answer ;(. Correct answer was 'no'\nLet's try again, {$name}\n");
                               break;
-                      case 'no':
-                              line("'no' is wrong answer ;(. Correct answer was 'yes'\n Let's try again, {$name}");
+                case 'no':
+                    exit("'no' is wrong answer ;(. Correct answer was 'yes'\nLet's try again, {$name}\n");
                               break;
-                      }
-            } elseif ($answer === $correctAnswer) {
-		    line('Correct!');
             }
-    
+        } elseif ($answer === $correctAnswer) {
+                line('Correct!');
+        }
     }
     line("Congratulations, %s!", $name);
-
 }
-
