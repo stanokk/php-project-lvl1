1424a374ab0d670bb9b1a5f882fb0bddd53b79c7
Evgeny Kuzin <ekuzin@outlook.com>
2021-06-22 20:45:56 +0700

    add Engine.php

---
+++ src/Engine.php
@@ -0,0 +1,20 @@
+<?php
+
+  namespace Brain\Games\Engine;
+
+  use function cli\line;
+  use function cli\prompt;
+
+  
+function playGame($rule, $round, $congrats)
+{
+    line('Welcome to the Brain Game!');
+    $name = prompt('May I have your name?');
+    line("Hello, %s!", $name);
+    line($rule);
+    $round($name);
+    line($congrats, $name);
+}
+
+
+
