adc56286df59d847f39992c5109a1ea9770035f5
Evgeny Kuzin <ekuzin@outlook.com>
2021-06-23 09:40:59 +0700

    Update Even.php

--- src/Even.php
+++ src/Even.php
@@ -8,35 +8,34 @@
 
 function startEvenGame()
 {
-	$rule = 'Answer "yes" if the number is even, otherwise answer "no".';
-	$congrats = "Congratulations, %s!";
+        $rule = 'Answer "yes" if the number is even, otherwise answer "no".';
+        $congrats = "Congratulations, %s!";
 
 
-$round = function($name) 
-{
-    for ($i = 0; $i <= 2; $i++) {
-         $number = mt_rand(1, 100);
-         line("Question: {$number}");
-         $answer = prompt('Your answer');
+    $round = function ($name) {
+        for ($i = 0; $i <= 2; $i++) {
+            $number = mt_rand(1, 100);
+            line("Question: {$number}");
+            $answer = prompt('Your answer');
 
-        if ($number % 2 === 0) {
-            $correctAnswer = 'yes';
-        } elseif ($number % 2 !== 0) {
-            $correctAnswer = 'no';
-        }
-        if ($answer !== $correctAnswer) {
-            switch ($answer) {
-                case 'yes':
-                    exit("'yes' is wrong answer ;(. Correct answer was 'no'\nLet's try again, {$name}\n");
-                              break;
-                case 'no':
-                    exit("'no' is wrong answer ;(. Correct answer was 'yes'\nLet's try again, {$name}\n");
-                              break;
+            if ($number % 2 === 0) {
+                $correctAnswer = 'yes';
+            } elseif ($number % 2 !== 0) {
+                $correctAnswer = 'no';
             }
-        } elseif ($answer === $correctAnswer) {
+            if ($answer !== $correctAnswer) {
+                switch ($answer) {
+                    case 'yes':
+                        exit("'yes' is wrong answer ;(. Correct answer was 'no'\nLet's try again, {$name}\n");
+                              break;
+                    case 'no':
+                        exit("'no' is wrong answer ;(. Correct answer was 'yes'\nLet's try again, {$name}\n");
+                              break;
+                }
+            } elseif ($answer === $correctAnswer) {
                 line('Correct!');
+            }
         }
-    }
-};
+    };
     playGame($rule, $round, $congrats);
 }
