0c30b7fb74451bfe22a87d332dc85a9ed6d9d767
Evgeny Kuzin <ekuzin@outlook.com>
2021-07-16 15:02:52 +0700

    Update Engine.php ; Calc.php

--- src/Engine.php
+++ src/Engine.php
@@ -5,11 +5,25 @@ namespace Brain\Games\Engine;
 use function cli\line;
 use function cli\prompt;
 
-function playGame(string $rule, callable $round): void
+function playGame(string $rule, string $question, string $correctAnswer): void
 {
     line('Welcome to the Brain Game!');
     $name = prompt('May I have your name?');
     line("Hello, %s!", $name);
     line($rule);
-    $round($name);
+    for ($i = 0; $i <= 2; $i++) {
+        line($question);
+        $answer = prompt('Your answer');
+        if ($answer === $correctAnswer) {
+                line('Correct!');
+        }
+        if ($answer !== $correctAnswer) {
+                        line("'{$answer}' is wrong answer ;(. Correct answer was '{$correctAnswer}'.
+Let's try again, {$name}!");
+                        break;
+        }
+        if ($i === 2) {
+                line("Congratulations, %s!", $name);
+        }
+    }
 }
--- src/Games/Calc.php
+++ src/Games/Calc.php
@@ -23,31 +23,25 @@ function calcResult(string $someOperator, int $firstNum, int $secondNum): int
     return $result;
 }
 
+function setQuestion(int $number1, int $number2, string $character): string
+{
+    return "Question: {$number1} {$character} {$number2}";
+}
+
+function getRule(): string
+{
+    return 'What is the result of the expression?';
+}
+
+
 function startCalcGame(): void
 {
-    $rule = 'What is the result of the expression?';
-
-    $round = function ($name): void {
-        for ($i = 0; $i <= 2; $i++) {
-            $num1 = mt_rand(1, 20);
-            $num2 = mt_rand(1, 20);
-            $operator = ['+', '-', '*'];
-            $randomOperator = $operator[array_rand($operator)];
-            line("Question: {$num1} {$randomOperator} {$num2}");
-            $answer =  prompt('Your answer');
-            $correctAnswer = calcResult($randomOperator, $num1, $num2);
-            if ($answer === (string) $correctAnswer) {
-                line('Correct!');
-            }
-            if ($answer !== (string) $correctAnswer) {
-                        line("'{$answer}' is wrong answer ;(. Correct answer was '{$correctAnswer}'.
-Let's try again, {$name}!");
-                        break;
-            }
-            if ($i === 2) {
-                line("Congratulations, %s!", $name);
-            }
-        }
-    };
-    playGame($rule, $round);
+    $num1 = mt_rand(1, 20);
+    $num2 = mt_rand(1, 20);
+    $operator = ['+', '-', '*'];
+    $randomOperator = $operator[array_rand($operator)];
+    $rule = getRule();
+    $question = setQuestion($num1, $num2, $randomOperator);
+    (string) $correctAnswer = calcResult($randomOperator, $num1, $num2);
+    playGame($rule, $question, $correctAnswer);
 }
