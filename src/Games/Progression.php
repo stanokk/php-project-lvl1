43bdf16e7fcd052242cc693bf20513e8dd33c666
Evgeny Kuzin <ekuzin@outlook.com>
2021-07-23 12:49:31 +0700

    Update Progression.php

--- src/Games/Progression.php
+++ src/Games/Progression.php
@@ -6,38 +6,41 @@ use function cli\line;
 use function cli\prompt;
 use function Brain\Games\Engine\playGame;
 
+function getRule(): string
+{
+    return 'What number is missing in the progression?';
+}
+
+function getProgression(int $num, int $step): array
+{
+    $arr = [];
+    $arr[0] = $num;
+    for ($j = 1; $j < 10; $j++) {
+        $arr[$j] = $arr[$j - 1] + $step;
+    }
+    return $arr;
+}
+
+function setQuestion(string $str): string
+{
+    return "Question: {$str}";
+}
+
 function startProgressionGame(): void
 {
-    $rule = 'What number is missing in the progression?';
-
-    $round = function ($name): void {
-
-        for ($i = 0; $i <= 2; $i++) {
+    $rule = getRule();
+    $round = function (): array {
+            $array = [];
             $firstNum = mt_rand(1, 20);
             $progressionStep = mt_rand(1, 10);
-            $array = [];
-            $array[0] = $firstNum;
-            for ($j = 1; $j < 10; $j++) {
-                    $array[$j] = $array[$j - 1] + $progressionStep;
-            }
-            $hiddenIndex = array_rand($array);
-            $hiddenValue = $array[$hiddenIndex];
-            $array[$hiddenIndex] = '..';
-            $arrayAsString = implode(' ', $array);
-
-            line("Question: {$arrayAsString}");
-            $answer =  prompt('Your answer');
-            if ($answer === (string) $hiddenValue) {
-                line('Correct!');
-            } else {
-                        line("'{$answer}' is wrong answer ;(. Correct answer was '{$hiddenValue}'.
-Let's try again, {$name}!\n");
-                break;
-            }
-            if ($i === 2) {
-                line("Congratulations, %s!", $name);
-            }
-        }
+            $progression = getProgression($firstNum, $progressionStep);
+            $hiddenIndex = array_rand($progression);
+            $correctAnswer = $progression[$hiddenIndex];
+            $progression[$hiddenIndex] = '..';
+            $progressionAsString = implode(' ', $progression);
+            $question = setQuestion($progressionAsString);
+            $array[$question] = $correctAnswer;
+            return $array;
     };
     playGame($rule, $round);
 }
